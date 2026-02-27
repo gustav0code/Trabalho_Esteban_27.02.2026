a7fa57ff961e521ff58e7d2a469b25b7ab19fc4f
gustav0code <gustavosantosf10@gmail.com>
2026-02-27 16:29:41 -0300

    Atividade 1

---
+++ AT1.cs
@@ -0,0 +1,20 @@
+﻿//Atividade 1
+
+double distancia,x1, x2, y1, y2;
+
+Console.WriteLine("Digite o x1");
+x1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o y1");
+y1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o x2");
+x2 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o y2");
+y2 = double.Parse(Console.ReadLine());  
+
+
+distancia = Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+
+Console.WriteLine("A distância dos pontos é " + distancia);
