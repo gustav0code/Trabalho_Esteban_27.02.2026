1fdf0525c5902f5e59e92b989a17586cd7634259
gustav0code <gustavosantosf10@gmail.com>
2026-02-27 16:33:34 -0300

    Atividade 4

---
+++ AT4.cs
@@ -0,0 +1,17 @@
+﻿//atividade 4
+
+int n;
+
+Console.WriteLine("Digite um número inteiro");
+n = int.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine(n + " é um número par");
+}
+else
+{
+    Console.WriteLine(n + " é um número impar");
+}
+
+Console.ReadLine();
